c55e8d129277546d8470f80a0ffd187ac249cd7f
beatrixyu <beatrixyu@126.com>
2021-05-17 17:12:13 +0200

    add

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,24 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Login />
+    <Register/>
+
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/Login.vue'
+import Register from './components/Register.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Login,
+    Register
   }
 }
 </script>
---
+++ src/components/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="login">
+    <h1>Login</h1>
+     <form v-on:reset="resetFields" v-on:submit.prevent="submitForm">
+       <div class="form-row">
+         <div class="form-field">
+          <!-- <label for="first-name">First Name</label> -->
+          <input type="text" id="first-name" v-model.trim="firstName" placeholder="Vorname*"/>
+        </div>
+      </div>
+      <div class="form-row">
+        <div class="form-field">
+          <!-- <label for="email">Email</label> -->
+          <input type="text" id="email" v-model.trim="email" placeholder="E-Mail Adresse*"/>
+        </div>
+      </div>
+       <div class="form-row">
+        <div class="form-field">
+          <!-- <label for="password">Password</label> -->
+          <input type="number" id="email" v-model.trim="email" placeholder="Passwort*"/>
+        </div>
+      </div>
+     </form>     
+  </div>
+</template>
