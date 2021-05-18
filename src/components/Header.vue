00b2dacbb8e1a2b412908d454c604b6eb15cff45
beatrixyu <beatrixyu@126.com>
2021-05-18 23:24:22 +0200

    router fixed

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,22 @@
 <template>
-  <div id="header" class="grid justify-items-stretch h-16">
+  <div id="header" class="grid justify-items-stretch h-22">
       <div class="flex">
       <div class="w-1/2 cursor-pointer">
          <a href="https://www.justspices.de/" target="_blank"><img class="w-12 ml-6" alt="company logo" src="../assets/images/logo.png"></a> 
       </div>
       <div class="flex justify-start items-center w-1/2 pr-10">
-          <div class="w-1/7 mr-2">
-              <img class="w-7" alt="trust logo" src="../assets/images/safe.png" />
+          <div class="w-1/7 mr-2 flex justify-center">
+              <img class="w-6" alt="trust logo" src="../assets/images/safe.png" />
           </div>
           <div class="w-6/7">
-              <div class="flex">
-                  <img class="w-3 h-2.6 mr-0.5" alt="stars" src="../assets/images/yellowStar.png"/>
-                  <img class="w-3 h-2.6 mr-0.5" alt="stars" src="../assets/images/yellowStar.png"/>
-                  <img class="w-3 h-2.6 mr-0.5" alt="stars" src="../assets/images/yellowStar.png"/>
-                  <img class="w-3 h-2.6 mr-0.5" alt="stars" src="../assets/images/yellowStar.png"/>
-                  <img class="w-3 h-2.6 mr-0.5" alt="stars" src="../assets/images/yellowStar.png"/>
+              <div class="flex flex-row" >
+                  <img class="w-3 h-auto mr-0.5" alt="stars" src="../assets/images/yellowStar.png"/>
+                  <img class="w-3 h-auto mr-0.5" alt="stars" src="../assets/images/yellowStar.png"/>
+                  <img class="w-3 h-auto mr-0.5" alt="stars" src="../assets/images/yellowStar.png"/>
+                  <img class="w-3 h-auto mr-0.5" alt="stars" src="../assets/images/yellowStar.png"/>
+                  <img class="w-3 h-auto mr-0.5" alt="stars" src="../assets/images/yellowStar.png"/>
               </div>
-              <div class="flex items-center">
+              <div class="flex flex-row items-center">
                   <div class="w-2/5">
                     <span class="inline-block align-middle text-xs tracking-tighter"> 4.85 / 5.00</span>
                   </div>
